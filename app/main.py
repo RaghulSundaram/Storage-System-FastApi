4b63d1a9d8c07cfa87726ebec68765356140d73f
Raghul <raghulsun2@gmail.com>
2022-01-30 13:36:04 +0530

    error-Nonetype

--- app/main.py
+++ app/main.py
@@ -43,7 +43,7 @@ async def authenticate_user(username: str, password: str):
     return user
 
 
-def create_access_token(data: dict, expires_delta: timedelta | None = None):
+def create_access_token(data: dict, expires_delta: timedelta):
     to_encode = data.copy()
     if expires_delta:
         expire = datetime.utcnow() + expires_delta
